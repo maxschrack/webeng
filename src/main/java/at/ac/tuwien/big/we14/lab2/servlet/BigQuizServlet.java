e5a85df296c95ee9891289526ca0416c9bd7dc19
Alexei <alexei.zamyatin@yahoo.de>
2014-04-11 13:20:33 +0200

    angabe

---
+++ src/main/java/at/ac/tuwien/big/we14/lab2/servlet/BigQuizServlet.java
@@ -0,0 +1,14 @@
+package at.ac.tuwien.big.we14.lab2.servlet;
+
+import javax.servlet.http.*;
+
+
+
+public class BigQuizServlet extends HttpServlet{
+	
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response){
+		
+	}
+}
+// Your Servlet implementation
